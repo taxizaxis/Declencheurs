ad0c95953b97ace3521ee5f1e1c99a3136dd35ea
taxizaxis <romaindacas@outlook.fr>
2017-09-20 23:21:28 +0200

    ajout des listes

--- Declencheurs/Views/AccueilDetailControl.xaml.cs
+++ Declencheurs/Views/AccueilDetailControl.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-
 using Declencheurs.Models;
-
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -32,8 +30,16 @@ namespace Declencheurs.Views
         {
             SetCheckedState();
         }
+        private void OptionComposant_Checked(object sender, RoutedEventArgs e)
+        {
+            SetCheckedState();
+        }
 
         private void Option_Unchecked(object sender, RoutedEventArgs e)
+        {
+            SetCheckedState();
+        }
+        private void OptionComposant_Unchecked(object sender, RoutedEventArgs e)
         {
             SetCheckedState();
         }
@@ -51,7 +57,28 @@ namespace Declencheurs.Views
             CheckboxLundi.IsChecked = CheckboxMardi.IsChecked = CheckboxMercredi.IsChecked = CheckboxJeudi.IsChecked = CheckboxVendredi.IsChecked = CheckboxSamedi.IsChecked = CheckboxDimanche.IsChecked = true;
         }
 
+        private void SelectAllComposant_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox CheckboxWiFi = sender as CheckBox;
+            CheckBox CheckboxBluetooth = sender as CheckBox;
+            CheckBox CheckboxDonnéesMobiles = sender as CheckBox;
+
+            CheckboxLundi.IsChecked = CheckboxMardi.IsChecked = CheckboxMercredi.IsChecked = CheckboxJeudi.IsChecked = CheckboxVendredi.IsChecked = CheckboxSamedi.IsChecked = CheckboxDimanche.IsChecked = true;
+        }
+
         private void SelectAll_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBox CheckboxLundi = sender as CheckBox;
+            CheckBox CheckboxMardi = sender as CheckBox;
+            CheckBox CheckboxMercredi = sender as CheckBox;
+            CheckBox CheckboxJeudi = sender as CheckBox;
+            CheckBox CheckboxVendredi = sender as CheckBox;
+            CheckBox CheckboxSamedi = sender as CheckBox;
+            CheckBox CheckboxDimanche = sender as CheckBox;
+
+            CheckboxLundi.IsChecked = CheckboxMardi.IsChecked = CheckboxMercredi.IsChecked = CheckboxJeudi.IsChecked = CheckboxVendredi.IsChecked = CheckboxSamedi.IsChecked = CheckboxDimanche.IsChecked = false;
+        }
+        private void SelectAllComposant_Unchecked(object sender, RoutedEventArgs e)
         {
             CheckBox CheckboxLundi = sender as CheckBox;
             CheckBox CheckboxMardi = sender as CheckBox;
@@ -94,7 +121,36 @@ namespace Declencheurs.Views
                 CheckboxAll.IsChecked = false;
             }
         }
+        private void SelectAllComposant_Indeterminate(object sender, RoutedEventArgs e)
+        {
+            // If the SelectAll box is checked (all options are selected), 
+            // clicking the box will change it to its indeterminate state.
+            // Instead, we want to uncheck all the boxes,
+            // so we do this programatically. The indeterminate state should
+            // only be set programatically, not by the user.
+            CheckBox CheckboxLundi = sender as CheckBox;
+            CheckBox CheckboxMardi = sender as CheckBox;
+            CheckBox CheckboxMercredi = sender as CheckBox;
+            CheckBox CheckboxJeudi = sender as CheckBox;
+            CheckBox CheckboxVendredi = sender as CheckBox;
+            CheckBox CheckboxSamedi = sender as CheckBox;
+            CheckBox CheckboxDimanche = sender as CheckBox;
+            CheckBox CheckboxAll = sender as CheckBox;
 
+
+            if (CheckboxLundi.IsChecked == true &&
+                CheckboxMardi.IsChecked == true &&
+                CheckboxMercredi.IsChecked == true &&
+                CheckboxJeudi.IsChecked == true &&
+                CheckboxVendredi.IsChecked == true &&
+                CheckboxSamedi.IsChecked == true &&
+                CheckboxDimanche.IsChecked == true)
+            {
+                // This will cause SelectAll_Unchecked to be executed, so
+                // we don't need to uncheck the other boxes here.
+                CheckboxAll.IsChecked = false;
+            }
+        }
         private void SetCheckedState()
         {
 
